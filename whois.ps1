cbc6a71ea97aabe1ef94aac98d08bc1e40dc3f9f
esun-ian <77189423+esun-ian@users.noreply.github.com>
2021-06-24 11:22:03 +0800

    新增 proxy  認證連線以及等待2秒

--- whois.ps1
+++ whois.ps1
@@ -1,7 +1,24 @@
 ﻿$OUTPUTFILE="Result.txt"
 Remove-Item $OUTPUTFILE -Force
 
+#$user = $(whoami).Split('\')[1]
+$user = $(whoami)
+echo "使用登入開機帳號驗證 Proxy: $user"
+
+$passwd = Read-Host "請輸入登入開機密碼: " -AsSecureString
+$myCreds=New-Object System.Management.Automation.PSCredential -ArgumentList "$user",$passwd
+
+Set-ItemProperty -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" ProxyEnable -value 1
+
+
 foreach($line in Get-Content .\HostNames.txt) {
     echo $line
-    start powershell -argument ".\query.ps1 $line" -wait -NoNewWindow
+    $result = $(curl https://www.nigsun.net/lg/whois.php?domain=$line -Proxy 'http://172.17.12.36:8080'  -ProxyCredential $myCreds)
+    $result.Content.Split("`n") | ForEach-Object {
+        if($_ -match "Record created" -or $_ -match "Creation Date" -or $_ -match "No Found" -or $_ -match "No match" ) {
+            echo "$line $_" >> $OUTPUTFILE;             
+        };
+    }
+    echo "等待2秒..."
+    start-sleep -s 2
 }
