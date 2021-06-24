8b07252e9e54e33be2445404aada4cec4b1dde0e
aeifkz <aeifkz@gmail.com>
2021-06-24 09:28:59 +0800

    因為斷線問題調整查詢方式

--- whois.ps1
+++ whois.ps1
@@ -3,10 +3,5 @@ Remove-Item $OUTPUTFILE -Force
 
 foreach($line in Get-Content .\HostNames.txt) {
     echo $line
-    $result = $(curl https://www.nigsun.net/lg/whois.php?domain=$line)
-    $result.Content.Split("`n") | ForEach-Object {
-        if($_ -match "Record created" -or $_ -match "Creation Date" -or $_ -match "No Found" -or $_ -match "No match" ) {
-            echo "$line $_" >> $OUTPUTFILE;             
-        };
-    }
+    start powershell -argument ".\query.ps1 $line" -wait -NoNewWindow
 }
